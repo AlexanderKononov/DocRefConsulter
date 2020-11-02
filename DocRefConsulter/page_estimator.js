4b78fc3394665ed1ea6400053cb176aaa6a7ce9e
AlexanderKononov <Kononov.nov@mail.ru>
2020-11-02 00:44:34 +0000

    add the templait for app

---
+++ DocRefConsulter/page_estimator.js
@@ -0,0 +1,8 @@
+// Page estimator for DocRefConsulter extention
+
+function getRating(page_url){
+	chrome.tabs.executeScript(null,
+	{code:""})
+)
+
+document.addEventListener('DOMContentLoaded', function () {})
