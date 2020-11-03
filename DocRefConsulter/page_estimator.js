7e2a6f745b5570a07f0a706df49f738dc877fc17
AlexanderKononov <Kononov.nov@mail.ru>
2020-11-03 17:31:42 +0000

    fix popup page

--- DocRefConsulter/page_estimator.js
+++ DocRefConsulter/page_estimator.js
@@ -7,21 +7,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			var targetRating = getRating(activeTabUrl);
 			console.log("find_3");
 			console.log(targetRating);
-			//
-
-			//console.log(targetRating)
 	});
-
-
-async function getRatingLibrary() {
-	const response = await fetch('rating_library.csv');
-	const ratingLibrary = await response.text();
-	//console.log(ratingLibrary);
-	return ratingLibrary;
-};
-
-//console.log(getRatingLibrary());
-
 
 async function getRating(page_url) {
 	console.log("page_estimator_script");
@@ -32,51 +18,24 @@ async function getRating(page_url) {
 	const ratingLibrary = await response.text();
 
 
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			var activeTabUrl = tabs[0].url;
-			console.log("in_query");
-			//var targetRating = getRating(activeTabUrl);
+	let pUrl = page_url.replace("https://", "").replace("http://", "").replace("www.", "");
 
-			let pUrl = activeTabUrl.replace("https://", "").replace("http://", "").replace("www.", "");
-
-			const rows = ratingLibrary.split('\n').slice(1);
-			let targetRating = -1;
-	
-
-
-			rows.forEach(elt => {
-				const row = elt.split(',');
-				let a = row[0].replace("https://", "").replace("http://", "").replace("www.", "");
-				a = a.split('/')[0];
-				let b = pUrl.replace("https://", "").replace("http://", "").replace("www.", "");
-				b = b.split('/')[0];
-				console.log(a + "vs." + b);
-				if(a == b) {
-					console.log("find");
-					targetRating = row[1];
-					//console.log(targetRating);
-				};
-		
-			});
-			console.log("find_2");
-			console.log(targetRating);
-
-			console.log("find_3");
-			console.log(targetRating);
-			document.getElementById("rating").textContent = targetRating
-			document.write("<h4>" + targetRating + "</h4>");
-
-			//
-
-			//console.log(targetRating)
+	const rows = ratingLibrary.split('\n').slice(1);
+	let targetRating = -1;
+	rows.forEach(elt => {
+		const row = elt.split(',');
+		let a = row[0].replace("https://", "").replace("http://", "").replace("www.", "");
+		a = a.split('/')[0];
+		let b = pUrl.replace("https://", "").replace("http://", "").replace("www.", "");
+		b = b.split('/')[0];
+		console.log(a + "vs." + b);
+		if(a == b) {
+			console.log("find");
+			targetRating = row[1];
+		};
 	});
-
+	console.log("find_2");
+	console.log(targetRating);
+	document.getElementById("rating").innerHTML = targetRating;
 	console.log(page_url);
-	
-
-	
-	return targetRating;
-
-};
-
-//getCurrentUrl()
+}
