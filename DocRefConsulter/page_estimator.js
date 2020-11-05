e5dfdd28dbd6c3fe7db2305059216981740d1936
AlexanderKononov <Kononov.nov@mail.ru>
2020-11-05 10:46:48 +0000

    add doc and community rating scors

--- DocRefConsulter/page_estimator.js
+++ DocRefConsulter/page_estimator.js
@@ -2,9 +2,9 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			var activeTabUrl = tabs[0].url;
 			console.log("in_query");
-			var targetRating = getRating(activeTabUrl);
+			var targRat = getRating(activeTabUrl);
 			console.log("find_3");
-			console.log(targetRating);
+			console.log(targRat);
 });
 
 async function getRating(page_url) {
@@ -17,18 +17,29 @@ async function getRating(page_url) {
 	let pUrl = page_url.replace("https://", "").replace("http://", "").replace("www.", "");
 	pUrl = pUrl.split('/')[0];
 	const rows = ratingLibrary.split('\n').slice(1);
-	let targetRating = -1;
+	let targRat = [0,0,0,0];
 	rows.forEach(elt => {
 		const row = elt.split(',');
 		let a = row[0].replace("https://", "").replace("http://", "").replace("www.", "");
 		a = a.split('/')[0];
 		if(a == pUrl) {
 			console.log("find");
-			targetRating = row[1];
+			targRat = [parseInt(row[1], 10), parseInt(row[2], 10),
+				parseInt(row[3], 10), parseInt(row[4], 10)];
 		};
 	});
+	console.log(targRat);
+	targRat.push((targRat[0]+ targRat[1]+targRat[2]+targRat[3])/4);
+
 	document.getElementById("page_domen").innerHTML = pUrl;
-	document.getElementById("inform_rating").innerHTML = targetRating;
-	document.getElementById("support_rating").innerHTML = targetRating;
+	if(targRat[4] > 0) {
+		document.getElementById("curated_score_inf").innerHTML = targRat[0];
+		document.getElementById("curated_score_supp").innerHTML = targRat[1];
+		document.getElementById("community_score_inf").innerHTML = targRat[2];
+		document.getElementById("community_score_supp").innerHTML = targRat[3];
+		document.getElementById("tot_score").innerHTML = targRat[4];
+	}
+	
+	
 	console.log(page_url);
 }
